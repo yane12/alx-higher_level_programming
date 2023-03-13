0897b8f2c66a7ffb54e85084370c8883cb43dae1
yane12 <yane12girma@gmail.com>
2023-03-13 07:05:12 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print('{:d}'.format(i))
+def no_c(my_string):
+    new_str = ''
+    for i in my_string:
+        if i != 'C' and i != 'c':
+            new_str += i
+
+    return new_str
