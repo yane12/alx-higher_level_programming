5c3a6208d342d0084ed170d5bbb7b7a0b57cbe7d
yaniG <yane12girma@gmail.com>
2024-01-17 10:04:57 -0500

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
---creates a database named hbtn_0d_usa containing a table named cities
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-use hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    state_id INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id),
-    name VARCHAR(256) NOT NULL
+-- Creates the database hbtn_0d_usa with the table cities.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+    PRIMARY KEY(`id`),
+    `id`       INT          NOT NULL AUTO_INCREMENT,
+    `state_id` INT          NOT NULL,
+    `name`     VARCHAR(256) NOT NULL,
+    FOREIGN KEY(`state_id`)
+    REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
