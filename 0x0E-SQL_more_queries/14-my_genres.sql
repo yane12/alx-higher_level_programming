66155b3e949ec87bcefe7cf4abde0f8de495dd94
yaniG <yane12girma@gmail.com>
2024-01-17 10:00:07 -0500

    Create 14-my_genres.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,2 @@
+-- Get genres of the show Dexter
+SELECT name FROM tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id JOIN tv_genres ON genre_id = tv_genres.id WHERE title = "Dexter" ORDER BY name ASC;
