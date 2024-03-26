088ef9afa521418120f52545d333e88332de6e53
yaniG <yane12girma@gmail.com>
2024-03-26 02:38:54 -0400

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 // Read and print the content of a file
 
 const fileReader = require('fs');
-const file = process.argv[2];
+const fileName = process.argv[2];
 
-fileReader.readFile(file, 'utf8', function (err, data) {
+fileReader.readFile(fileName, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
