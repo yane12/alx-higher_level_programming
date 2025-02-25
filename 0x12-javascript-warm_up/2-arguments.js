64f71465d8f2093a046b48aba79a23c2f512dc5e
yaniG <yane12girma@gmail.com>
2025-02-25 05:33:13 +0000

    update 2-args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const process = require('node:process')
 const argc = process.argv.length;
 const values = ['No argument', 'Argument found', 'Arguments found'];
 console.log(values[Math.min(argc - 2, 2)]);
