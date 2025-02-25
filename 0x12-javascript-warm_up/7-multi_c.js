b9073f3f605d84a1a7607ccb8103141e614585d3
yane12 <yane12girma@gmail.com>
2025-02-25 00:30:25 -0800

    update 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 const x = parseInt(process.argv[2]);
-
-for (let i = 0; i < x; i++) {
-  console.log('C is fun');
+if ( typeof(x) === 'number' && !isNaN(x)) {
+	if ( x > 0 ) {
+		for (let i = 0; i < x; i++) {
+  			console.log('C is fun');
+		}
+	}
+}
+else {
+	console.log('Missing number of occurrences');
 }
