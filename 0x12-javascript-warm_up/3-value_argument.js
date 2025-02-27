92b29f4f68616002fdd6a9657e1394890104010c
yane12 <yane12girma@gmail.com>
2025-02-27 09:38:58 -0800

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let i = 1;
-const values = ['No argument', process.argv[2]];
-if (values[1] === undefined) {
-  i--;
-}
-console.log(values[i]);
+const res =  process.argv[2] ?? "No argument";
+console.log(res);
+
